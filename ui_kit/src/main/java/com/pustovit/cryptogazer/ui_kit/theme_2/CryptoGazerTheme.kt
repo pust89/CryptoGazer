009d38a502ceb742ad371e164e1a6c5283f7aec5
Vladimir Pustovit <vovapust1989@gmail.com>
2026-03-20 13:30:49 +0300

    create CryptoGazerTypography

--- ui_kit/src/main/java/com/pustovit/cryptogazer/ui_kit/theme_2/CryptoGazerTheme.kt
+++ ui_kit/src/main/java/com/pustovit/cryptogazer/ui_kit/theme_2/CryptoGazerTheme.kt
@@ -1,33 +1,31 @@
 package com.pustovit.cryptogazer.ui_kit.theme_2
 
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Shapes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.pustovit.cryptogazer.ui_kit.theme_2.palette.CryptoGazerPalette
+import com.pustovit.cryptogazer.ui_kit.theme_2.palette.CryptoGazerPaletteLight
+import com.pustovit.cryptogazer.ui_kit.theme_2.type.CryptoGazerTypography
 
 object CryptoGazerTheme {
 
-//    /**
-//     * Retrieves the current [ColorScheme] at the call site's position in the hierarchy.
-//     *
-//     * @sample androidx.compose.material3.samples.ThemeColorSample
-//     */
-//    val colorScheme: ColorScheme
-//        @Composable @ReadOnlyComposable get() = LocalColorScheme.current
-//
-//    /**
-//     * Retrieves the current [Typography] at the call site's position in the hierarchy.
-//     *
-//     * @sample androidx.compose.material3.samples.ThemeTextStyleSample
-//     */
-//    val typography: Typography
-//        @Composable @ReadOnlyComposable get() = LocalTypography.current
-//
-//    /**
-//     * Retrieves the current [Shapes] at the call site's position in the hierarchy.
-//     *
-//     * @sample androidx.compose.material3.samples.ThemeShapeSample
-//     */
-//    val shapes: Shapes
-//        @Composable @ReadOnlyComposable get() = LocalShapes.current
+    val palette: CryptoGazerPalette
+        @Composable @ReadOnlyComposable get() = LocalCryptoGazerPalette.current
+
+    val typography: CryptoGazerTypography
+        @Composable @ReadOnlyComposable get() = LocalCryptoGazerTypography.current
+
+    /**
+     * Retrieves the current [Shapes] at the call site's position in the hierarchy.
+     *
+     * @sample androidx.compose.material3.samples.ThemeShapeSample
+     */
+    val shapes: Shapes
+        @Composable @ReadOnlyComposable get() = LocalShapes.current
 }
+
+internal val LocalCryptoGazerPalette =
+    staticCompositionLocalOf<CryptoGazerPalette> { CryptoGazerPaletteLight }
+
+internal val LocalCryptoGazerTypography = staticCompositionLocalOf { CryptoGazerTypography }
