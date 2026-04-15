785a968ec5fe4c365bbdfd3ea55fb0eeb0065df7
Vladimir Pustovit <vovapust1989@gmail.com>
2026-04-15 13:37:48 +0300

    save

--- onboarding/src/main/java/com/pustovit/cryptogazer/onboarding/tea/OnboardingUiConverter.kt
+++ onboarding/src/main/java/com/pustovit/cryptogazer/onboarding/tea/OnboardingUiConverter.kt
@@ -1,10 +1,36 @@
 package com.pustovit.cryptogazer.onboarding.tea
 
 import com.pustovit.cryptogazer.onboarding.OnboardingUiState
+import com.pustovit.cryptogazer.ui_kit.card.CardState
+import com.pustovit.cryptogazer.ui_kit.text.AppTextState
+import com.pustovit.cryptogazer.ui_kit.theme_2.type.AppTypography
 
 class OnboardingUiConverter {
 
     fun convert(state: OnboardingState): OnboardingUiState {
-        return OnboardingUiState.Loading
+        return when {
+            state.loading -> OnboardingUiState.Loading
+            else -> getOnboardingUiState(state)
+        }
+    }
+
+    private fun getOnboardingUiState(
+        state: OnboardingState
+    ): OnboardingUiState {
+        return OnboardingUiState.Details(
+            cards = state.cards.map {
+                CardState(
+                    id = it.id,
+                    title = AppTextState(
+                        text = it.cardTitle,
+                        style = AppTypography.titleMedium
+                    ),
+                    description = AppTextState(
+                        text = it.cardDescription,
+                        style = AppTypography.bodyMedium
+                    ),
+                )
+            }
+        )
     }
 }
